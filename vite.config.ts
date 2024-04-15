9f7e9251282906f7ba1bb7af8ce20b7623098231
ChienChihYeh <miroyeh1986@gmail.com>
2024-04-15 14:36:28 +0800

    workaround for bug with Cornerstone3D tools with vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import wasm from "vite-plugin-wasm";
 
 // https://vitejs.dev/config/
+// https://github.com/cornerstonejs/cornerstone3D/issues/1071 current workaround for bug with vite and Cornerstone3D tools when build
 export default defineConfig({
-  plugins: [vue()],
-})
+  plugins: [vue(), wasm()],
+  resolve: {
+    alias: {
+      "@cornerstonejs/tools": "@cornerstonejs/tools/dist/umd/index.js",
+    },
+  },
+  worker: {
+    plugins() {
+      return [wasm()];
+    },
+  },
+});
